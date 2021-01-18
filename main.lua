b850cb8e9b742645caa5c03db698a520aaa4b49f
Bgabri <76167293+Bgabri@users.noreply.github.com>
2021-01-18 22:19:59 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,131 +1,125 @@
+-- https://imgur.com/gallery/lMrgZ
+
 local fps = 0
-local width = love.graphics.getWidth()
-local height = love.graphics.getHeight()
-local triangles = {}
-local nextVertix = 1
-local circles = {}
-
+local terrain = {}
+local test = {10, 10,10,10}
 local scale = 10
-
+local height = math.sqrt(3)/2*scale
+local numV = love.graphics.getHeight()/height
+local numH = love.graphics.getWidth()/scale - 1
+local off = 0
 function love.load()
-	for i=1, 100 do
-		addCircle(math.random()*width, math.random()*height, 1, (100-math.random()*150), math.pi/5)
+	love.math.setRandomSeed(1)
+	for i = 1, numV do
+		terrain[i] = {}
+		for j = 1, numH do
+			terrain[i][j] = 0
+			-- terrain[i][j] = math.floor(love.math.random()*2*2)/4
+			-- terrain[i][j] = math.ceil(1-(love.math.noise((j/numH))+love.math.noise((love.math.noise(i/numV)))))
+			-- terrain[i][j] = (1.1-(love.math.noise((j/numH))+love.math.noise((love.math.noise(i/numV)))))*10
+			-- terrain[i][j] = (1-(math.ceil((love.math.noise((j/numH))+love.math.noise((love.math.noise(i/numV)))-1)*5)/3))
+			if (terrain[i][j] < 0 ) then
+				terrain[i][j] = 0
+			elseif (terrain[i][j] > 1 ) then
+				terrain[i][j] = 1
+			end
+
+			if (i == math.floor(numV/2)) then
+				terrain[i][j] = 1
+			end
+		end	
 	end
 
-	for i=1, height/(math.sqrt(3)/2*scale)-1 do
-		for j=1, width/scale-1 do
-			height = math.sqrt(3)/2*scale
-			local x1, y1 = (j+i%2/2)*scale, (i)*height
-			addTriangle(x1, y1, x1-0.5*scale, y1+height, x1+0.5*scale, y1+height)
-			addTriangle(x1, y1, x1+scale, y1, x1+0.5*scale, y1+height)
-		end
-	end
+	terrain[10][11] = 1
+	terrain[11][10] = 1
+	terrain[11][11] = 1
+	terrain[10][12] = 0.2
+
+	local V = math.floor(numV/2)
+	V = V+1-V%2
+	local H = math.floor(numH/2)
+
+	terrain[V][H] = 1
+	terrain[V][H+1] = 1
+	terrain[V][H+2] = 1
+	-- terrain[V][H+3] = 0.1
+
+	terrain[V+1][H] = 1
+	terrain[V+2][H-1] = 1
+	terrain[V+3][H] = 1
+	terrain[V+4][H] = 1
+	terrain[V+4][H+1] = 1
+	terrain[V+4][H+2] = 1
+	terrain[V+3][H+3] = 1
+	terrain[V+2][H+3] = 1
+	terrain[V+1][H+3] = 1
+
+
+	-- terrain[6][12] = 1
+	-- terrain[7][11] = 1
+	-- terrain[7][12] = 1
 end
 
 function love.update(delta)
-	fps = love.timer.getFPS()
-	for i,v in ipairs(circles) do
-		v[1] = v[1] + math.sin(v[5])*v[4]*delta
-		v[2] = v[2] + math.cos(v[5])*v[4]*delta
-		v[5] = v[5] + delta
-		if (v[5] > math.pi) then
-			v[5] = -v[5]
-		end
-	end
+fps = love.timer.getFPS()
 end
 
 function love.draw()
-	for i,v in ipairs(circles) do
-		for j, v2 in ipairs(triangles) do
-			xPos, yPos = (v2[1] + v2[3] + v2[5])/3, (v2[2] + v2[4] + v2[6])/3
-			d = math.sqrt((xPos-v[1])^2+(yPos-v[2])^2)
-			if (d < scale) then
-				if(triangleCircleCollision(v2[1], v2[2], v2[3], v2[4], v2[5], v2[6], v[1], v[2], v[3])) then
-					love.graphics.polygon("line", v2)
-					break
-				end
-			end
+	love.graphics.setPointSize(2)
+	for i, row in ipairs(terrain) do
+		for j, v in ipairs(row) do
+			love.graphics.setColor(v+0.1, v+0.1, v+0.1)
+			local x, y = (j + i%2/2)*scale, i*height
+			love.graphics.points(x, y)
 		end
-		love.graphics.circle("line", v[1], v[2], v[3])
+	end
+	love.graphics.setPointSize(1)
+	love.graphics.setColor(0.4, 0.5, 1, 1)
+	for i = 1, #terrain-1, 1 do
+		for j = 2-i%2, #terrain[i]-1, 1 do
+			local x, y = (j + i%2/2)*scale, i*height
+			isoUp(terrain[i][j], terrain[i+1][j+i%2-1], terrain[i+1][j+i%2], x, y)
+			isoDown(terrain[i][j], terrain[i][j+1], terrain[i+1][j+i%2], x, y)
+		end
 	end
 	love.graphics.setColor(0.0, 1.0, 0.1)
 	love.graphics.print(fps, 0, 0)
 end
 
-function love.mousepressed(x, y, button, istouch)
-	if button == 1 then
- 		triangles[1][nextVertix*2-1] = x
- 		triangles[1][nextVertix*2] = y
- 		nextVertix = nextVertix + 1
- 		if(nextVertix > 3) then
- 			nextVertix = 1
- 		end
+function newValue(value)
+	if (value == nil) then
+		value = 0
 	end
+	return value
 end
 
-function pointOnTriangle(px, py, ax, ay, bx, by, cx, cy)
-	local abx, aby = bx - ax, by - ay
-	local acx, acy = cx - ax, cy - ay
-	local apx, apy = px - ax, py - ay
-	-- vertex region outside a
-	local d1 = abx*apx + aby*apy
-	local d2 = acx*apx + acy*apy
-	if d1 <= 0 and d2 <= 0 then
-		return ax, ay
-	end
-	-- vertex region outside b
-	local bpx, bpy = px - bx, py - by
-	local d3 = abx*bpx + aby*bpy
-	local d4 = acx*bpx + acy*bpy
-	if d3 >= 0 and d4 <= d3 then
-		return bx, by
-	end
-	-- edge region ab
-	if d1 >= 0 and d3 <= 0 and d1*d4 - d3*d2 <= 0 then
-		local v = d1/(d1 - d3)
-		return ax + abx*v, ay + aby*v
-	end
-	-- vertex region outside c
-	local cpx, cpy = px - cx, py - cy
-	local d5 = abx*cpx + aby*cpy
-	local d6 = acx*cpx + acy*cpy
-	if d6 >= 0 and d5 <= d6 then
-		return cx, cy
-	end
-	-- edge region ac
-	if d2 >= 0 and d6 <= 0 and d5*d2 - d1*d6 <= 0 then
-		local w = d2/(d2 - d6)
-		return ax + acx*w, ay + acy*w
-	end
-	-- edge region bc
-	if d3*d6 - d5*d4 <= 0 then
-		local d43 = d4 - d3
-		local d56 = d5 - d6
-		if d43 >= 0 and d56 >= 0 then
-			local w = d43/(d43 + d56)
-			return bx + (cx - bx)*w, by + (cy - by)*w
-		end
-	end
-	-- inside face region
-	return px, py
+function isoUp(top, bottomLeft, bottomRight, x, y)
+	shiftX = 0.5*scale
+
+	local intraValue = 0.5
+	local value = (intraValue-top)/(bottomRight-top) -- right
+	local x1 = x + shiftX*value
+	local y1 = y + height*value
+	local value = (bottomRight-intraValue)/(bottomLeft-bottomRight) -- bottom
+	local x2 = x+shiftX + 2*(shiftX)*value
+	local y2 = y + height
+	local value = (intraValue-top)/(bottomLeft-top) -- left
+	local x3 = x - shiftX*value
+	local y3 = y + height*value
+	return(x1, y1, x2, y2, x3, y3)
 end
 
-function pointInCircle(px, py, cx, cy, r)
-	local dx, dy = px - cx, py - cy
-	return dx*dx + dy*dy <= r*r
-end
-
-function triangleCircleCollision(ax, ay, bx, by, cx, cy, sx, sy, r)
-	local qx, qy = pointOnTriangle(sx, sy, ax, ay, bx, by, cx, cy)
-	return pointInCircle(qx, qy, sx, sy, r)
-end -- stolen from 2dengine.com
-
-function addCircle(x, y, r, v, t)
-	local circle= {x, y, r, v, t}
-	table.insert(circles, circle)
-end
-
-function addTriangle(x1, y1, x2, y2, x3, y3)
-	local triangle = {x1, y1, x2, y2, x3, y3}
-	table.insert(triangles, triangle)
+function isoDown(topLeft, topRight, bottom, x, y)
+	shiftX = 0.5*scale
+	local intraValue = 0.5
+	local value = (intraValue-topLeft)/(topRight-topLeft) -- right
+	local x1 = x + scale*value
+	local y1 = y
+	local value = (intraValue-topRight)/(bottom-topRight)
+	local x2 = x + scale + (shiftX-scale)*value
+	local y2 = y + height*value
+	local value = (intraValue-topLeft)/(bottom-topLeft)
+	local x3 = x + shiftX*value
+	local y3 = y + height*value
+	return(x1, y1, x2, y2, x3, y3)
 end
